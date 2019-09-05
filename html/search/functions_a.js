b500a16c437815900f82bc700b25e682f82a4187
lhamot <loic.hamot@yahoo.fr>
2019-09-06 00:34:40 +0200

    Add min_element and max_element

--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -5,5 +5,7 @@ var searchData=
   ['make_5fpipeable',['make_pipeable',['../namespacerah.html#a9beb2a94a054fd0caefd5a20b1c0f0d9',1,'rah']]],
   ['map_5fkey',['map_key',['../namespacerah_1_1view.html#a273917511589d75b32d28b56b7de2655',1,'rah::view::map_key(R &amp;&amp;range)'],['../namespacerah_1_1view.html#aac8d9437b8c96e0e8e972c4c10d584e5',1,'rah::view::map_key()']]],
   ['map_5fvalue',['map_value',['../namespacerah_1_1view.html#a7896dcebf13a4294254ed87743ba3ad0',1,'rah::view::map_value(R &amp;&amp;range)'],['../namespacerah_1_1view.html#ad11fcca5438e227541594a917a85f77c',1,'rah::view::map_value()']]],
+  ['max_5felement',['max_element',['../namespacerah.html#a84983065bc08e1416b730260a0ab033c',1,'rah::max_element(R &amp;&amp;range)'],['../namespacerah.html#a6553b7209f85e70a671b3c3c6153d255',1,'rah::max_element()'],['../namespacerah.html#a46840028da6087e1ae3348b1ce8ce381',1,'rah::max_element(R &amp;&amp;range, P &amp;&amp;pred)'],['../namespacerah.html#a8e6f0498df081347423d319c8ab919d1',1,'rah::max_element(P &amp;&amp;pred)']]],
+  ['min_5felement',['min_element',['../namespacerah.html#ac30d42caf315f29ed784dd3c4f46d533',1,'rah::min_element(R &amp;&amp;range)'],['../namespacerah.html#a8b5a2712dbebe5144ea1635f3f346514',1,'rah::min_element()'],['../namespacerah.html#ae2ea3fdd33de831fcc7b281154d5a37c',1,'rah::min_element(R &amp;&amp;range, P &amp;&amp;pred)'],['../namespacerah.html#a28dcdb4c5a25916a18532f1dbb520aa9',1,'rah::min_element(P &amp;&amp;pred)']]],
   ['mismatch',['mismatch',['../namespacerah.html#a5ce2d92c6f0b3dd5eb6d70600e949a97',1,'rah']]]
 ];
