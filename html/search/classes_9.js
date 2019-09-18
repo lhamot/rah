9d118836881fec61fdea750d7105ac0fa31bb5e9
lhamot <loic.hamot@yahoo.fr>
2019-09-19 00:20:29 +0200

    update doc

--- html/search/classes_9.js
+++ html/search/classes_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['set_5fdifference_5fiterator',['set_difference_iterator',['../structrah_1_1view_1_1set__difference__iterator.html',1,'rah::view']]],
+  ['sliding_5fiterator',['sliding_iterator',['../structrah_1_1view_1_1sliding__iterator.html',1,'rah::view']]],
   ['stream_5finserter_5fiterator',['stream_inserter_iterator',['../structrah_1_1stream__inserter__iterator.html',1,'rah']]],
   ['stride_5fiterator',['stride_iterator',['../structrah_1_1view_1_1stride__iterator.html',1,'rah::view']]],
   ['subrange',['SubRange',['../structrah_1_1view_1_1join__iterator_1_1_sub_range.html',1,'rah::view::join_iterator']]]
--- html/search/variables_9.js
+++ html/search/variables_9.js
@@ -3,6 +3,8 @@ var searchData=
   ['step_5f',['step_',['../structrah_1_1view_1_1iota__iterator.html#ac30abc689d8d82810bf78ceec3893bd2',1,'rah::view::iota_iterator::step_()'],['../structrah_1_1view_1_1stride__iterator.html#a1b2e6cfb0631cde24284f56b17a4452c',1,'rah::view::stride_iterator::step_()'],['../structrah_1_1view_1_1chunk__iterator.html#a71025fe393b81b113b2773063c4126be',1,'rah::view::chunk_iterator::step_()']]],
   ['stream_5f',['stream_',['../structrah_1_1stream__inserter__iterator.html#ab8594076013c695b314616093e8fdb4c',1,'rah::stream_inserter_iterator']]],
   ['subrange_5f',['subRange_',['../structrah_1_1view_1_1join__iterator.html#ae9351284af0f64c368606315da0b56a5',1,'rah::view::join_iterator::subRange_()'],['../structrah_1_1view_1_1join__iterator.html#ac4ca6ff3a0816533a5aa0cd49dbb630d',1,'rah::view::join_iterator::subRange_()']]],
+  ['subrangebegin_5f',['subRangeBegin_',['../structrah_1_1view_1_1sliding__iterator.html#a4d921b6315b961a731e955cd57c66046',1,'rah::view::sliding_iterator']]],
   ['subrangeend',['subRangeEnd',['../structrah_1_1view_1_1join__iterator_1_1_sub_range.html#a59768b4f12beb31f228a01dad4618c8c',1,'rah::view::join_iterator::SubRange']]],
-  ['subrangeiter',['subRangeIter',['../structrah_1_1view_1_1join__iterator_1_1_sub_range.html#a4073148b86fc958e754b23af1befce93',1,'rah::view::join_iterator::SubRange']]]
+  ['subrangeiter',['subRangeIter',['../structrah_1_1view_1_1join__iterator_1_1_sub_range.html#a4073148b86fc958e754b23af1befce93',1,'rah::view::join_iterator::SubRange']]],
+  ['subrangelast_5f',['subRangeLast_',['../structrah_1_1view_1_1sliding__iterator.html#a6ad809caec4abec1b0248c86c72c5087',1,'rah::view::sliding_iterator']]]
 ];
